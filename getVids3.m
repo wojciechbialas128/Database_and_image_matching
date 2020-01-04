2ea857e4f46b1cba031669b77a9b3a7797c61035
wojciechbialas128 <59510731+wojciechbialas128@users.noreply.github.com>
2020-01-04 23:01:05 +0100

    Add files via upload

--- getVids3.m
+++ getVids3.m
@@ -1,6 +1,7 @@
 function [depthVid,colorVid, gigeThermoVid] = getVids3()
 clear
 tic
+    isOK =1;
     try
         colorVid = videoinput('kinect',1, 'RGB_1280x960');
         depthVid = videoinput('kinect',2, 'Depth_640x480');
@@ -9,15 +10,18 @@ tic
         depthVid.FramesPerTrigger = 1;
         %start([colorVid depthVid]);
     catch
-        depthVid=0;
-        colorVid=0;
-        disp ('Unable to connect to kinect');       
+        disp ('Unable to connect to kinect'); 
+        isOK=0;
     end
     try 
         gigeThermoVid=gigecam(1);
     catch
-        gigeThermoVid=0;
         disp('Unable to connect to Flir 325sc');
+        isOK=0;
+    end
+    if isOK==0
+        clear
+        error('Unable to continue program, please connect device'); 
     end
 end
 
--- save3images.m
+++ save3images.m
@@ -1,7 +1,22 @@
 function [] = save3images(imgd,imgc,imgt,timed,timec,timet)
-    dd='D:\Studia_files\INZYNIERKA_FILES\Newest_records\Depth_img\';
-    dc='D:\Studia_files\INZYNIERKA_FILES\Newest_records\Color_img\';
-    dt='D:\Studia_files\INZYNIERKA_FILES\Newest_records\Thermo_img\';
+  	dir_code = cd;
+    cd ..;
+    dir_main = cd;
+    dir_images = [dir_main '\Multimodal image']; 
+    
+    if  isfolder([dir_main '/Multimodal image'])==0
+        mkdir ('Multimodal image');  
+        disp(['Created new dirctionaries in: ' dir_images]);
+        cd (dir_images)
+        mkdir ('Depth_img');
+        mkdir ('Color_img');
+        mkdir ('Thermo_img');
+    end  
+    cd (dir_code);
+
+    dd=[dir_images '\Depth_img\'];
+    dc=[dir_images '\Color_img\'];
+    dt=[dir_images '\Thermo_img\'];
     dd=dd+string(timed)+'.png';
     dc=dc+string(timec)+'.png';
     dt=dt+string(timet)+'.png';
